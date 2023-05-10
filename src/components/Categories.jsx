1527a0aafd7283ce24f7ee6ab86d22b76249b3ca
Meghann Stamper <meg.stamp@yahoo.com>
2023-05-10 12:40:57 -0400

    buttons and modals

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,17 +1,37 @@
 import { useEffect, useState } from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Modal, Button } from "react-bootstrap";
 
-
-//fetch will be inside useEffect
-//data  .filter by category
-//in useEffect once all db is returned, .filter to separate and store like categories, we are going to store them in 3 state variables
+// function BlockButton() {
+//     return (
+//       <div>
+//         <Button variant="primary" size="lg">
+//           Block level button
+//         </Button>
+//         <Button variant="secondary" size="lg">
+//           Block level button
+//         </Button>
+//       </div>
+//     );
+//   }
+//   export default BlockButton;
 
 
 export default function Categories() {
     const [artists, setArtists] = useState()
     const [tudors, setTudors] = useState()
     const [femaleLeaders, setFemaleLeaders] = useState()
-    
+    const [openModal, setOpenModal] = useState(false)
+    const [thisFigure, setThisFigure] = useState()
+
+    const handleOpen = (person) => {
+        setThisFigure(person)
+        setOpenModal(true)
+    }
+
+    const handleClose = () => {
+        setOpenModal(false)
+    }
+
 
     useEffect(() => {
         fetch("https://how-they-look-today-api.web.app/figures")
@@ -30,43 +50,57 @@ export default function Categories() {
                 <Row className="text-center">
 
                     <Col sm={12} md={4}>
-                    <h2>Artists</h2>
-                        {!artists
-                         ? "loading"
-                            : artists.map(
-                                (artist) => (
-                                    <div 
-                                        key={artist.id}>
-                                        {artist.person}
-                                    </div>) )}
+                        <h2>Artists</h2>
+                            {!artists
+                            ? "loading"
+                                : artists.map(
+                                    (artist) => (
+                                        <Button class="bg-secondary" 
+                                        onClick={
+                                            () => handleOpen(artist)}
+                                            key={artist.id}>
+                                            {artist.person}
+                                        </Button>) )}
                     </Col>
 
                     <Col sm={12} md={4}>
-                    <h2>Tudors</h2>
-                        {!tudors
-                        ? "loading"
-                            : tudors.map(
-                                (tudors) => (
-                                    <div
-                                        key={tudors.id}>
-                                            {tudors.person}
-                                    </div>))}
+                        <h2>Tudors</h2>
+                            {!tudors
+                            ? "loading"
+                                : tudors.map(
+                                    (tudor) => (
+                                        <Button onClick={
+                                            () => handleOpen(tudor)}
+                                            key={tudor.id}>
+                                            {tudor.person}
+                                        </Button>))}
                     </Col>
 
                     <Col sm={12} md={4}>
-                    <h2>Female Leaders</h2>
-                        {!femaleLeaders
-                        ? "loading"
-                            : femaleLeaders.map(
-                                (femaleLeaders) => (
-                                    <div
-                                        key={femaleLeaders.id}>
-                                            {femaleLeaders.person}
-                                    </div>)  ) }
+                        <h2>Female Leaders</h2>
+                            {!femaleLeaders
+                            ? "loading"
+                                : femaleLeaders.map(
+                                    (femaleLeader) => (
+                                        <Button onClick={
+                                            () => handleOpen(femaleLeader)}
+                                            key={femaleLeader.id}>
+                                            {femaleLeader.person}
+                                        </Button>)  ) }
                     </Col>
 
                 </Row>
-            </Container>
+            <Modal 
+                size="lg"
+                  show={openModal} onHide={handleClose}> 
+                <Modal.Body><p>{thisFigure?.person}</p> 
+                <img src={thisFigure?.picture} alt={thisFigure?.name} />
+                {thisFigure?.bio}
+                </Modal.Body>
+
+            </Modal>
+            </Container> 
+
         </section>
     )
 }
